9e49dc9a1e854a57d6517e65cda77f189f0797d9
aarunsoman <arunsoman@gmail.com>
2016-09-19 10:40:15 +0530

    bug fix

--- compiler/src/main/java/com/flytxt/parser/compiler/parser/ParserUtils.java
+++ compiler/src/main/java/com/flytxt/parser/compiler/parser/ParserUtils.java
@@ -10,9 +10,14 @@ public class ParserUtils {
 	protected String  getValue(String s){
 
 		String val = s.substring(s.indexOf('(')+1, s.length()-1);
+		try{
 		int valI = Integer.parseInt(val);
 		if (valI>maxVal)
 			maxVal = valI;
-		return s.substring(s.indexOf('(')+1, s.length()-1);
+			System.out.println("Number:"+val);
+		}catch (NumberFormatException e) {
+			System.out.println(val);
+		}
+		return val; 
 	}
 }
