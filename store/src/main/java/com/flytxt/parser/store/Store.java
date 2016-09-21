fc91b60ea3152294aa5de2b8152e81b47a41a351
Athul Raj <athul.raj@flytxt.com>
2016-09-21 23:51:04 +0530

    System is now fail-safe
    Output is written into a .tmp file, which will be renamed upon
    completion. During startup, all .tmp files are deleted from output
    directory and the partially completed file is parsed from the beginning.

--- store/src/main/java/com/flytxt/parser/store/Store.java
+++ store/src/main/java/com/flytxt/parser/store/Store.java
@@ -3,6 +3,8 @@ package com.flytxt.parser.store;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.file.DirectoryStream;
+import java.nio.file.DirectoryStream.Filter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -32,10 +34,31 @@ public class Store {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public Store(final String file, final String... headers) {
-        this.fileName = file;
+    public final static String TMP = ".tmp";
+
+    public Store(final String fileName, final String... headers) {
+        this.fileName = fileName;
         this.headers = headers;
+        deleteTempFile();
         createFile();
+    }
+
+    private void deleteTempFile() {
+        final Filter<Path> filter = new Filter<Path>() {
+
+            @Override
+            public boolean accept(final Path entry) throws IOException {
+                return entry.toFile().toString().endsWith(TMP);
+            }
+        };
+        final Path folder = Paths.get(fileName).getParent();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(folder, filter)) {
+            for (final Path entry : stream) {
+                entry.toFile().delete();
+            }
+        } catch (final IOException ex) {
+
+        }
     }
 
     private void createFile() {
@@ -51,8 +74,7 @@ public class Store {
                 return;
             }
         }
-        final String[] tmp = fileName.split("\\.");
-        final String name = tmp[0] + System.currentTimeMillis() + "." + tmp[1];
+        final String name = fileName + TMP;
         fileNameP = Paths.get(name);
         try {
             channel = new RandomAccessFile(fileNameP.toString(), "rw");
@@ -71,6 +93,11 @@ public class Store {
             status = -1;
             this.e = e;
         }
+        // catch (final BufferOverflowException e) {
+        // logger.error("Increase byte buffer size, current size :" + bBuff.capacity());
+        // status = -1;
+        // TODO handle this
+        // }
     }
 
     public void save(final byte[] data, final String fileName, final Marker... markers) throws IOException {
@@ -95,5 +122,8 @@ public class Store {
 
     public void done() throws IOException {
         channel.close();
+        final String[] tmp = fileName.split("\\.");
+        final String doneFile = tmp[0] + System.currentTimeMillis() + "." + tmp[1];
+        Files.move(Paths.get(fileName + TMP), Paths.get(doneFile));
     }
 }
