44532348a360ea0c611ffae2d4d2555749f83249
Arun Soman <aarunsoman@gmail.com>
2016-09-26 23:08:59 +0530

    Update .travis-install.sh

--- .travis-install.sh
+++ .travis-install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install makefs
-sudo apt-get install openjdk-8-jdk
+#sudo apt-get install openjdk-8-jdk
 
 # Build and install rumprun toolchain from source
 RUMPRUN_PLATFORM=${RUMPRUN_PLATFORM:-hw}
