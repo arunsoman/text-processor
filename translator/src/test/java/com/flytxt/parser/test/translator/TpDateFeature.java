2d4465a031e5438e906242f592592ff0e979c56a
Arun Soman <aarunsoman@gmail.com>
2016-09-29 19:11:17 +0530

    integrated

--- translator/src/test/java/com/flytxt/parser/test/translator/TpDateFeature.java
+++ translator/src/test/java/com/flytxt/parser/test/translator/TpDateFeature.java
@@ -1,54 +1,72 @@
 package com.flytxt.parser.test.translator;
 
-import cucumber.api.PendingException;
+import static org.junit.Assert.assertEquals;
+
+import com.flytxt.parser.marker.Marker;
+import com.flytxt.parser.marker.MarkerFactory;
+import com.flytxt.parser.translator.TpDate;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class TpDateFeature {
-
+	TpDate tpDate;
+	MarkerFactory mf;
+	Marker marker1;
+	Marker marker2;
+	boolean result;
+	String resultStr;
+	
 	@Given("^marker and tpdate class$")
 	public void markerAndTpdateClass() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		 mf = new MarkerFactory();
+		 tpDate = new TpDate();
+	}
+
+	private Marker getMarker(String str) {
+		Marker mocker = new Marker();
+		mocker.index = 0;
+		mocker.length = str.length();
+		return mocker;
 	}
 
 	@When("^\"([^\"]*)\" after \"([^\"]*)\"$")
 	public void after(String arg1, String arg2) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		byte[] d1 = arg1.getBytes();
+		byte[] d2 = arg2.getBytes();
+		result = tpDate.after(d1, getMarker(arg1), d2, getMarker(arg2));
 	}
 
 	@Then("^date one after date two returns \"([^\"]*)\"$")
 	public void dateOneAfterDateTwoReturns(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 	
 
 	@When("^\"([^\"]*)\" before \"([^\"]*)\"$")
 	public void before(String arg1, String arg2) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		byte[] d1 = arg1.getBytes();
+		byte[] d2 = arg2.getBytes();
+		result = tpDate.before(d1, getMarker(arg1), d2, getMarker(arg2));
 	}
 	
 	@Then("^date one before date two returns \"([^\"]*)\"$")
 	public void dateOneBeforeDateTwoReturns(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 
 	@When("^\"([^\"]*)\" minus \"([^\"]*)\"$")
 	public void minus(String arg1, String arg2) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		byte[] d1 = arg1.getBytes();
+		byte[] d2 = arg2.getBytes();
+		resultStr = ""+tpDate.differenceInMillis(d1, getMarker(arg1), d2, getMarker(arg2));
 	}
 
 	@Then("^diff is \"([^\"]*)\"$")
 	public void diffIs(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(arg1, resultStr);
 	}
 }
--- translator/src/test/java/com/flytxt/parser/test/translator/TpStringFeature.java
+++ translator/src/test/java/com/flytxt/parser/test/translator/TpStringFeature.java
@@ -1,136 +1,136 @@
 package com.flytxt.parser.test.translator;
 
+import static org.junit.Assert.assertEquals;
+
+import com.flytxt.parser.marker.Marker;
+import com.flytxt.parser.marker.MarkerFactory;
+import com.flytxt.parser.translator.TpString;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class TpStringFeature {
+	MarkerFactory mf;
+	Marker marker1;
+	Marker marker2;
+	boolean result;
+	String resultStr;
+	TpString tpString;
 
+	private Marker getMarker(String str) {
+		Marker mocker = new Marker();
+		mocker.index = 0;
+		mocker.length = str.length();
+		return mocker;
+	}
 	@Given("^marker and tpstring class$")
 	public void markerAndTpstringClass() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		mf = new MarkerFactory();
+		tpString = new TpString();
 	}
 
 	@When("^\"([^\"]*)\" provided$")
 	public void provided(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		resultStr = ""+tpString.length(arg1.getBytes(), getMarker(arg1), mf);
 	}
 
 	@Then("^return length \"([^\"]*)\"$")
 	public void returnLength(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(arg1, resultStr);
 	}
 
 	@When("^\"([^\"]*)\" starts with prefix \"([^\"]*)\"$")
 	public void startsWithPrefix(String arg1, String arg2) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		result = tpString.startsWtih(arg1.getBytes(), getMarker(arg1), arg2.getBytes(), getMarker(arg2));
 	}
 
 	@Then("^return true if string starts with prefix\"([^\"]*)\"$")
 	public void returnTrueIfStringStartsWithPrefix(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 	@When("^convert to upperCase\"([^\"]*)\"$")
 	public void convertToUpperCase(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		resultStr = tpString.toUpperCase(arg1.getBytes(), getMarker(arg1), mf).toString(arg1.getBytes());
 	}
 
 	@Then("^returns upperCase \"([^\"]*)\"$")
 	public void returnsUpperCase(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 	@When("^convert to lowerCase \"([^\"]*)\"$")
 	public void convertToLowerCase(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		resultStr = tpString.toLowerCase(arg1.getBytes(), getMarker(arg1), mf).toString(arg1.getBytes());
 	}
 
 	@Then("^returns lowerCase \"([^\"]*)\"$")
 	public void returnsLowerCase(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 	@When("^conver to titleCase\"([^\"]*)\"$")
 	public void converToTitleCase(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		resultStr = tpString.toTitleCase(arg1.getBytes(), getMarker(arg1), mf).toString(arg1.getBytes());
 	}
 
 	@Then("^returns titleCase \"([^\"]*)\"$")
 	public void returnsTitleCase(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 	@When("^\"([^\"]*)\" with leading whitespace is provided$")
 	public void withLeadingWhitespaceIsProvided(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		resultStr = tpString.lTrim(arg1.getBytes(), getMarker(arg1), mf).toString(arg1.getBytes());
 	}
 
 	@Then("^returns string without leading whitespaces \"([^\"]*)\"$")
 	public void returnsStringWithoutLeadingWhitespaces(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(arg1, resultStr);
 	}
 
 	@When("^\"([^\"]*)\" with trailing whitespaces provided$")
 	public void withTrailingWhitespacesProvided(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		resultStr = tpString.rTrim(arg1.getBytes(), getMarker(arg1), mf).toString(arg1.getBytes());
 	}
 
 	@Then("^return string without trailing whilespaces \"([^\"]*)\"$")
 	public void returnStringWithoutTrailingWhilespaces(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(arg1, resultStr);
 	}
 
 	@When("^\"([^\"]*)\" with whitespace in beginning or end is provided$")
 	public void withWhitespaceInBeginningOrEndIsProvided(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		resultStr = tpString.trim(arg1.getBytes(), getMarker(arg1), mf).toString(arg1.getBytes());
 	}
 
 	@Then("^return string with no whitespaces at start or end \"([^\"]*)\"$")
 	public void returnStringWithNoWhitespacesAtStartOrEnd(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(arg1, resultStr);
 	}
 
 	@When("^\"([^\"]*)\" contains \"([^\"]*)\"$")
 	public void contains(String arg1, String arg2) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		result = tpString.contains(arg1.getBytes(), getMarker(arg1),
+				arg2.getBytes(), getMarker(arg2),mf);
 	}
 
 	@Then("^return true if string contais subString\"([^\"]*)\"$")
 	public void returnTrueIfStringContaisSubString(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 	@When("^\"([^\"]*)\" contains \"([^\"]*)\" ignore case$")
 	public void containsIgnoreCase(String arg1, String arg2) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		result = tpString.containsIgnoreCase(arg1.getBytes(), getMarker(arg1),
+				arg2.getBytes(), getMarker(arg2),mf);
 	}
 
 	@Then("^return true if string contais subString ignoreCase \"([^\"]*)\"$")
 	public void returnTrueIfStringContaisSubStringIgnoreCase(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		assertEquals(result, arg1.equals("Y"));
 	}
 
 }
