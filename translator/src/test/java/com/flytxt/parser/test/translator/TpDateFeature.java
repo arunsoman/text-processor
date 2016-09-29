566932b60f654180bba4491d73516c79c0a71956
aslam <aslam.remeez@flytxt.com>
2016-09-29 18:26:47 +0530

    Remove duplicate methods in feature file

--- translator/src/test/java/com/flytxt/parser/test/translator/TpDateFeature.java
+++ translator/src/test/java/com/flytxt/parser/test/translator/TpDateFeature.java
@@ -19,23 +19,26 @@ public class TpDateFeature {
 		throw new PendingException();
 	}
 
-	@Then("^date(\\d+) after date(\\d+) returns \"([^\"]*)\"$")
-	public void dateAfterDateReturns(int arg1, int arg2, String arg3) throws Throwable {
+	@Then("^date one after date two returns \"([^\"]*)\"$")
+	public void dateOneAfterDateTwoReturns(String arg1) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
 		throw new PendingException();
 	}
+
+	
 
 	@When("^\"([^\"]*)\" before \"([^\"]*)\"$")
 	public void before(String arg1, String arg2) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
 		throw new PendingException();
 	}
-
-	@Then("^date(\\d+) before date(\\d+) returns \"([^\"]*)\"$")
-	public void dateBeforeDateReturns(int arg1, int arg2, String arg3) throws Throwable {
+	
+	@Then("^date one before date two returns \"([^\"]*)\"$")
+	public void dateOneBeforeDateTwoReturns(String arg1) throws Throwable {
 		// Write code here that turns the phrase above into concrete actions
 		throw new PendingException();
 	}
+
 
 	@When("^\"([^\"]*)\" minus \"([^\"]*)\"$")
 	public void minus(String arg1, String arg2) throws Throwable {
