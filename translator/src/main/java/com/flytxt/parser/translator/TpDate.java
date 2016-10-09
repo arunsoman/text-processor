6bc5042e7ab9244fd8d782b79f95c463dd19db31
aarunsoman <arunsoman@gmail.com>
2016-10-09 15:09:08 +0530

    convertDate implemented

--- translator/src/main/java/com/flytxt/parser/translator/TpDate.java
+++ translator/src/main/java/com/flytxt/parser/translator/TpDate.java
@@ -1,16 +1,17 @@
 package com.flytxt.parser.translator;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.HashMap;
 
 import com.flytxt.parser.marker.Marker;
 import com.flytxt.parser.marker.MarkerFactory;
+import com.flytxt.parser.translator.TpDateUtil.Translator;
 
 public class TpDate {
 
     public static String flyDateFormat = "ddMMyyyy HH:mm:ss";
 
-    private static final SimpleDateFormat sdf = new SimpleDateFormat(flyDateFormat);
+    private static TpDateUtil tpDateUtil = new TpDateUtil();  
 
     private static int[] yearArray = { 4, 5, 6, 7 };
 
@@ -19,9 +20,16 @@ public class TpDate {
     private static int[] monthArray = { 2, 3 };
 
     private static int[] time = { 9, 10, 12, 13, 15, 16 };
-
-    public Marker convertDate(final byte[] data, final Marker m, final MarkerFactory mf, final String format) {
-        throw new RuntimeException();
+    private static HashMap<String, Translator> planMap= new HashMap<String, Translator>();
+    
+    public Marker convertDate(final byte[] data, final Marker m, final MarkerFactory mf, final String format) throws ParseException {
+        Translator translator = planMap.get(format);
+        if(translator == null){
+        	translator = tpDateUtil.Formater(format);
+        	planMap.put(format, translator);
+        }
+        byte[] translate = translator.translate(data, null);
+        return mf.createImmutable(translate, 0, translate.length);
     }
 
     public boolean after(final byte[] data, final Marker m, final byte[] data2, final Marker m2) {
@@ -77,6 +85,6 @@ public class TpDate {
     public long differenceInMillis(final byte[] data, final Marker m, final byte[] data2, final Marker m2) throws ParseException {
         final byte[] d1 = (m.getData() != null) ? m.getData() : data;
         final byte[] d2 = (m2.getData() != null) ? m2.getData() : data2;
-        return sdf.parse(m.toString(d1)).getTime() - sdf.parse(m2.toString(d2)).getTime();
+        return tpDateUtil.parse(m.toString(d1)).getTime() - tpDateUtil.parse(m2.toString(d2)).getTime();
     }
 }
