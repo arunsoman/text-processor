9f3cbe3a1beb2e43159b17e009557324303103ee
Arun Soman <aarunsoman@gmail.com>
2016-10-13 10:51:20 +0530

    added profiles

---
+++ compiler/src/main/java/com/flytxt/parser/config/DataStoreConf.java
@@ -0,0 +1,36 @@
+package com.flytxt.parser.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class DataStoreConf {
+	@Primary
+	@Bean(name="datasource")
+	@ConfigurationProperties(prefix="test.datasource")
+	@Profile("test")
+	public DataSource forTest(){
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name="datasource")
+	@ConfigurationProperties(prefix="dev.datasource")
+	@Profile("dev")
+	public DataSource forDev(){
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name="datasource")
+	@ConfigurationProperties(prefix="prod.datasource")
+	@Profile("prod")
+	public DataSource forProd(){
+		return DataSourceBuilder.create().build();
+	}
+
+}
