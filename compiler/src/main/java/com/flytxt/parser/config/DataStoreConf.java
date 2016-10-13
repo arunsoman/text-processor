6f245acbb975a75e829b3602ff1286587d926eb1
Arun Soman <aarunsoman@gmail.com>
2016-10-13 12:33:35 +0530

    liquibase

--- compiler/src/main/java/com/flytxt/parser/config/DataStoreConf.java
+++ compiler/src/main/java/com/flytxt/parser/config/DataStoreConf.java
@@ -5,9 +5,11 @@ import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class DataStoreConf {
--- compiler/src/test/java/com/flytxt/parser/compiler/ParserDomainTest.java
+++ compiler/src/test/java/com/flytxt/parser/compiler/ParserDomainTest.java
@@ -2,16 +2,27 @@ package com.flytxt.parser.compiler;
 
 import java.io.File;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-// @SpringBootTest
+import com.flytxt.parser.config.DataStoreConf;
+import com.flytxt.parser.config.LiquibaseConf;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {Compiler.class, DataStoreConf.class, LiquibaseConf.class})
 public class ParserDomainTest {
 
     @Autowired
     private ParserDomain parserdomain;
 
-    // @Test
+    @Test
     public void testgetJars() throws Exception {
         File file = parserdomain.getJar("demo");
         System.out.println(file.length());
