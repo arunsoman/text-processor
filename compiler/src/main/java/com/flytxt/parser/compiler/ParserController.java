0bf8f82650e94a513d9f3e29b3f2c87bf9fb3ef3
Athul Raj <athul.raj@flytxt.com>
2016-10-14 18:34:07 +0530

    /compileNtest webservice accepts JSON as @RequestBody now

--- compiler/src/main/java/com/flytxt/parser/compiler/ParserController.java
+++ compiler/src/main/java/com/flytxt/parser/compiler/ParserController.java
@@ -11,10 +11,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.flytxt.parser.domain.CompileNTest;
 
 @Controller
 @EnableAutoConfiguration
@@ -23,19 +28,15 @@ public class ParserController {
     @Autowired
     private ParserDomain parserDomain;
 
-    @RequestMapping(path = "/compileNtest", method = RequestMethod.POST, produces = "text/plain")
-    public ResponseEntity<String> compileNtest(@RequestParam("name") final String name, @RequestParam("init") final String init, @RequestParam("absProcessor") String absProcessor,
-            @RequestParam("extract") final String extract, @RequestParam("transformation") final String transformation, @RequestParam("store") final String store,
-            @RequestParam("type") final String type, // single,hybrid
-            @RequestParam("sample") final String sampleData) {
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/compileNtest")
+    public ResponseEntity<String> compileNtest(@RequestBody final CompileNTest cNt) {
         String output;
         final HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
         try {
-            output = parserDomain.compileNtest(name, init, absProcessor, extract, transformation, store, type, sampleData);
-            // return ResponseEntity.ok().headers(headers).contentType(MediaType.parseMediaType("application/octet-stream")).body(new InputStreamResource(new
-            // ByteArrayInputStream((output).getBytes())));
+            output = parserDomain.compileNtest(cNt.getName(), cNt.getInit(), cNt.getAbsProcessor(), cNt.getExtract(), cNt.getTransformation(), cNt.getStore(), cNt.getType(), cNt.getSample());
         } catch (Exception e) {
             output = e.getMessage();
         }
