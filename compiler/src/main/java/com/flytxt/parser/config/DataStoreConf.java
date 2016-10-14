55f01f22c1e3217b7650d5d384abb0d12beac958
Athul Raj <athul.raj@flytxt.com>
2016-10-14 18:31:36 +0530

    Corrected spring configs

--- compiler/src/main/java/com/flytxt/parser/config/DataStoreConf.java
+++ compiler/src/main/java/com/flytxt/parser/config/DataStoreConf.java
@@ -5,34 +5,33 @@ import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class DataStoreConf {
-	@Primary
-	@Bean(name="datasource")
-	@ConfigurationProperties(prefix="test.datasource")
-	@Profile("test")
-	public DataSource forTest(){
-		return DataSourceBuilder.create().build();
-	}
 
-	@Bean(name="datasource")
-	@ConfigurationProperties(prefix="dev.datasource")
-	@Profile("dev")
-	public DataSource forDev(){
-		return DataSourceBuilder.create().build();
-	}
+    @Primary
+    @Bean(name = "datasource")
+    @ConfigurationProperties(prefix = "test.datasource")
+    @Profile("test")
+    public DataSource forTest() {
+        return DataSourceBuilder.create().build();
+    }
 
-	@Bean(name="datasource")
-	@ConfigurationProperties(prefix="prod.datasource")
-	@Profile("prod")
-	public DataSource forProd(){
-		return DataSourceBuilder.create().build();
-	}
+    @Bean(name = "datasource")
+    @ConfigurationProperties(prefix = "dev.datasource")
+    @Profile("dev")
+    public DataSource forDev() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "datasource")
+    @ConfigurationProperties(prefix = "prod.datasource")
+    @Profile("prod")
+    public DataSource forProd() {
+        return DataSourceBuilder.create().build();
+    }
 
 }
--- compiler/src/main/java/com/flytxt/parser/config/LiquibaseConf.java
+++ compiler/src/main/java/com/flytxt/parser/config/LiquibaseConf.java
@@ -3,6 +3,7 @@ package com.flytxt.parser.config;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
@@ -12,14 +13,15 @@ import liquibase.integration.spring.SpringLiquibase;
 
 @Configuration
 @EnableTransactionManagement
+@ComponentScan
 public class LiquibaseConf {
 
     @Profile("dev")
-    @Bean(name="liquibase")
+    @Bean(name = "liquibase")
     public SpringLiquibase liquibaseForDev(DataSource dataSource) {
         SpringLiquibase sl = new SpringLiquibase();
         sl.setDataSource(dataSource);
-        sl.setContexts("test");
+        sl.setContexts("dev");
         sl.setChangeLog("classpath:db/changelog/dev.dbchangelog.xml");
         sl.setShouldRun(true);
         return sl;
@@ -27,7 +29,7 @@ public class LiquibaseConf {
 
     @Primary
     @Profile("test")
-    @Bean(name="liquibase")
+    @Bean(name = "liquibase")
     public SpringLiquibase liquibaseForTest(DataSource dataSource) {
         SpringLiquibase sl = new SpringLiquibase();
         sl.setDataSource(dataSource);
