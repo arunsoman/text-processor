8e2c0b34b1ba0bac17b127faf0681307be8bc888
Athul Raj <athul.raj@flytxt.com>
2016-10-16 01:13:57 +0530

    Code cleanup

--- processor/src/main/java/com/flytxt/parser/processor/FolderEventListener.java
+++ processor/src/main/java/com/flytxt/parser/processor/FolderEventListener.java
@@ -4,7 +4,7 @@ import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
-import static java.nio.file.StandardWatchEventKinds.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -26,11 +26,9 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import com.sun.nio.file.SensitivityWatchEventModifier;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-
-import lombok.Data;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -42,6 +40,8 @@ import com.google.common.cache.LoadingCache;
 import com.google.common.cache.RemovalListener;
 import com.google.common.cache.RemovalListeners;
 import com.google.common.cache.RemovalNotification;
+
+import lombok.Data;
 
 @Component
 @ConfigurationProperties(prefix = "folder")
@@ -56,7 +56,7 @@ public class FolderEventListener {
 
     private LoadingCache<String, OutputUnit> cache;
 
-    private Map<WatchKey, List<OutputUnit>> hashCache = new HashMap<WatchKey, List<OutputUnit>>(10);
+    private Map<WatchKey, List<OutputUnit>> hashCache = new HashMap<>(10);
 
     private long cacheCleanUpTime;
 
@@ -74,14 +74,12 @@ public class FolderEventListener {
 
         private String destination;
 
-		public Watch(String source, String regex, String destination) {
-			super();
-			this.source = source;
-			this.regex = regex;
-			this.destination = destination;
-		}
-
-       
+        public Watch(String source, String regex, String destination) {
+            super();
+            this.source = source;
+            this.regex = regex;
+            this.destination = destination;
+        }
 
     }
 
@@ -114,14 +112,13 @@ public class FolderEventListener {
 
         }, 0, cacheCleanUpTime); // every 30s cache clean up will happen
 
-        for (final Watch w : ps.getFolderWatch()) {
+        for (final Watch w : ps.getFolderWatch())
             try {
                 attachFolder(w.source, w.regex, w.destination);
             } catch (final IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-        }
     }
 
     private class OutputUnit {
@@ -135,14 +132,13 @@ public class FolderEventListener {
         @SuppressWarnings("unused")
         private final Map<WatchKey, List<OutputUnit>> hashCache;
 
-
         public OutputUnit(final String regex, final String outFolder, final LoadingCache<String, OutputUnit> cache, final Map<WatchKey, List<OutputUnit>> hCache) {
             super();
             this.regex = regex;
             this.outFolder = outFolder;
             this.cache = cache;
             this.hashCache = hCache;
-           
+
         }
 
     }
@@ -153,7 +149,7 @@ public class FolderEventListener {
         try {
             outputUnits.add(new OutputUnit(regex, outfolder, cache, hashCache));
         } catch (final NullPointerException e) {
-            outputUnits = new ArrayList<OutputUnit>();
+            outputUnits = new ArrayList<>();
             outputUnits.add(new OutputUnit(regex, outfolder, cache, hashCache));
         }
         hashCache.put(key, outputUnits);
@@ -189,18 +185,15 @@ public class FolderEventListener {
         try {
 
             raf = new RandomAccessFile(file, "rw");
-            Files.createSymbolicLink(Paths.get(outputUnit.outFolder.concat("/").concat(file.getName())),
-					Paths.get(sourceFile));
+            Files.createSymbolicLink(Paths.get(outputUnit.outFolder.concat("/").concat(file.getName())), Paths.get(sourceFile));
         } catch (final IOException e) {
-            if (file.exists()) {
+            if (file.exists())
                 outputUnit.cache.put(sourceFile, outputUnit);
-            } else {
+            else
                 System.out.println("File was deleted while copying: '" + file.getAbsolutePath() + "'");
-            }
         } finally {
-            if (raf != null) {
+            if (raf != null)
                 raf.close();
-            }
         }
     }
 
@@ -225,10 +218,9 @@ public class FolderEventListener {
                     for (final WatchEvent<?> event : take.pollEvents()) {
                         final Kind<?> kind = event.kind();
 
-                        if (kind == OVERFLOW) {
+                        if (kind == OVERFLOW)
                             continue;
-                        }
-                        Path source =(Path)take.watchable();
+                        Path source = (Path) take.watchable();
                         final WatchEvent<Path> ev = cast(event);
                         final Path file = ev.context();
                         final String fileName = file.getFileName().toString();
@@ -237,19 +229,16 @@ public class FolderEventListener {
                         if (kind == ENTRY_CREATE) {
                             final List<OutputUnit> outputUnits = hashCache.get(take);
                             boolean found = false;
-                            for (final OutputUnit outputUnit : outputUnits) {
+                            for (final OutputUnit outputUnit : outputUnits)
                                 if (fileName.matches(outputUnit.regex)) {
-                                	outputUnit.cache.put(source.toString().concat("/").concat(fileName), outputUnit);
+                                    outputUnit.cache.put(source.toString().concat("/").concat(fileName), outputUnit);
                                     found = true;
                                 }
-                            }
-                            if (!found) {
+                            if (!found)
                                 System.out.println("no regx found for" + fileName);
-                            }
                         }
-                        if (kind == ENTRY_MODIFY) {
-                        	cache.get(source.toString().concat("/").concat(fileName));
-                        }
+                        if (kind == ENTRY_MODIFY)
+                            cache.get(source.toString().concat("/").concat(fileName));
                     }
                     take.reset();
                 }
