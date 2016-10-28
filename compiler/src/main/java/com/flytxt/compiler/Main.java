c23329bce4aeffd9a3e6a65e4dd57db3d53c8017
aarunsoman <arunsoman@gmail.com>
2016-10-28 07:26:17 +0530

    missed import

--- compiler/src/main/java/com/flytxt/compiler/Main.java
+++ compiler/src/main/java/com/flytxt/compiler/Main.java
@@ -2,8 +2,21 @@ package com.flytxt.compiler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.flytxt.parser.marker.MarkerDefaultConfig;
+import com.flytxt.parser.processor.ProcessorDefaultConfig;
+import com.flytxt.parser.store.StoreDefaultConfig;
+import com.flytxt.parser.translator.TranslatorDefaultConfig;
 
 @SpringBootApplication
+@ContextConfiguration(classes = {
+		MarkerDefaultConfig.class,
+		StoreDefaultConfig.class,
+		TranslatorDefaultConfig.class,
+		ProcessorDefaultConfig.class
+})
+
 public class Main {
 
     public static void main(String args[]) {
