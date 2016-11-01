90b6805868636acfcd3ef1e7186da7dece6ec377
aarunsoman <arunsoman@gmail.com>
2016-11-01 20:02:05 +0530

    flow set ;)

--- processor/src/main/java/com/flytxt/parser/processor/Job.java
+++ processor/src/main/java/com/flytxt/parser/processor/Job.java
@@ -2,12 +2,8 @@ package com.flytxt.parser.processor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Lob;
-import javax.persistence.PostLoad;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import lombok.Data;
 
@@ -16,13 +12,8 @@ import lombok.Data;
 @Table(name = "job_dk2")
 public class Job {
 
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    @Transient
-    private Long idValue;
-
+	private String name;
+	
     @Column(name = "active")
     private boolean active;
 
@@ -43,9 +34,4 @@ public class Job {
     @Lob
     @Column(name = "byte_code")
     private byte[] byteCode;
-    
-    @PostLoad
-    public void postload() {
-        this.idValue = this.id;
-    }
 }
--- processor/src/main/java/com/flytxt/parser/processor/ProxyScripts.java
+++ processor/src/main/java/com/flytxt/parser/processor/ProxyScripts.java
@@ -3,6 +3,7 @@ package com.flytxt.parser.processor;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -25,57 +26,60 @@ import lombok.Data;
 @Data
 public class ProxyScripts {
 
-    public String getScript;
+	public String getScript;
 
-    public String getJar;
+	public String getJar;
 
-    public String remoteHost;
+	public String remoteHost;
 
-    public String hostName;
+	public String hostName;
 
-    private List<LineProcessor> lps;
+	private List<LineProcessor> lps;
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private List<com.flytxt.parser.processor.FolderEventListener.Watch> folderWatch;
 
-    @Autowired
-    JobRepo repo;
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public List<LineProcessor> getLPInstance() {
-        return lps;
-    }
-    static class DbClassLoader extends ClassLoader{
-		Class <?>getClass(byte[] d){
-			return defineClass(d, 0, d.length);
+	@Autowired
+	JobRepo repo;
+
+	public List<LineProcessor> getLPInstance() {
+		return lps;
+	}
+
+	static class DbClassLoader extends ClassLoader {
+		Class<LineProcessor> getClass(byte[] d, String name) {
+			return (Class<LineProcessor>) defineClass(name, d, 0, d.length);
 		}
 	}
-    @PostConstruct
+
+	@PostConstruct
     public void init() throws Exception {
     	DbClassLoader loader = new DbClassLoader();
-    	List<Job> jobs = repo.findByhostNameAndActiveTrueAndStatusTrue(getHostname());
+    	String hostName = getHostname();
+    	logger.debug("who am i ? :"+ hostName);
+    	List<Job> jobs = repo.findByhostNameAndActiveTrueAndStatusTrue(hostName);
+    	folderWatch = new ArrayList<>(jobs.size());
     	for(Job aJob: jobs){
-    		lps.add((LineProcessor)loader.getClass(aJob.getByteCode()).newInstance());
+    		lps.add((LineProcessor)loader.getClass(aJob.getByteCode(), aJob.getName()).newInstance());
+    		com.flytxt.parser.processor.FolderEventListener.Watch w = new Watch(aJob.getInputPath(), aJob.getRegex(), aJob.getOutputPath());
+    		folderWatch.add(w);
     	}
     }
-    private String getHostname()
-    {
-      try (BufferedInputStream in = new BufferedInputStream(Runtime.getRuntime().exec("hostname").getInputStream()))
-      {
-        byte[] b = new byte[256];
-        in.read(b, 0, b.length); //guaranteed to read all data before returning
-        return new String(b);
-      }
-      catch (IOException e)
-      {
-        String message = "Error reading hostname";
-        throw new RuntimeException(message, e);
-      }
-    }
-    class FolderWatch {
-    }
 
-    @SuppressWarnings("unchecked")
-    public List<com.flytxt.parser.processor.FolderEventListener.Watch> getFolderWatch() {
-        
-        return null;
-    }
+	private String getHostname() {
+		try (BufferedInputStream in = new BufferedInputStream(Runtime.getRuntime().exec("hostname").getInputStream())) {
+			byte[] b = new byte[256];
+			in.read(b, 0, b.length); 
+			return new String(b);
+		} catch (IOException e) {
+			String message = "Error reading hostname";
+			throw new RuntimeException(message, e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<com.flytxt.parser.processor.FolderEventListener.Watch> getFolderWatch() {
+		return folderWatch;
+	}
 }
