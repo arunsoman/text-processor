969d044c20d57a69b53b5787f3b4d0b20966f82c
Athul Raj <athul.raj@flytxt.com>
2016-11-02 09:27:43 +0530

    Corrected test

--- translator/src/test/java/com/flytxt/parser/translator/feature/TpStringFeature.java
+++ translator/src/test/java/com/flytxt/parser/translator/feature/TpStringFeature.java
@@ -28,7 +28,6 @@ public class TpStringFeature extends TpAbsTest {
 
     TpString tpString;
 
-
     @Given("^marker and tpstring class$")
     public void markerAndTpstringClass() throws Throwable {
         markerFactory = new MarkerFactory();
@@ -37,7 +36,7 @@ public class TpStringFeature extends TpAbsTest {
 
     @When("^\"([^\"]*)\" provided$")
     public void provided(@Transform(MarkerTransform.class) final MarkerHelper mh) throws Throwable {
-        resultStr = "" + tpString.length( mh.getMarker(markerFactory));
+        resultStr = "" + tpString.length(mh.getMarker(markerFactory), markerFactory);
     }
 
     @Then("^check string result \"([^\"]*)\"$")
@@ -52,7 +51,7 @@ public class TpStringFeature extends TpAbsTest {
 
     @When("^\"([^\"]*)\" starts with prefix \"([^\"]*)\"$")
     public void startsWithPrefix(final String arg1, final String arg2) throws Throwable {
-        result = tpString.startsWith( getMarker(arg1),  getMarker(arg2));
+        result = tpString.startsWith(getMarker(arg1), getMarker(arg2));
     }
 
     @Then("^check boolean result \"([^\"]*)\"$")
@@ -80,7 +79,7 @@ public class TpStringFeature extends TpAbsTest {
 
     @When("^\"([^\"]*)\" with leading whitespace is provided$")
     public void withLeadingWhitespaceIsProvided(@Transform(MarkerTransform.class) final MarkerHelper mh) throws Throwable {
-        resultStr = tpString.lTrim( mh.getMarker(markerFactory), markerFactory).toString();
+        resultStr = tpString.lTrim(mh.getMarker(markerFactory), markerFactory).toString();
     }
 
     @Then("^returns string without leading whitespaces \"([^\"]*)\"$")
@@ -90,7 +89,7 @@ public class TpStringFeature extends TpAbsTest {
 
     @When("^\"([^\"]*)\" with trailing whitespaces provided$")
     public void withTrailingWhitespacesProvided(@Transform(MarkerTransform.class) final MarkerHelper mh) throws Throwable {
-        resultStr = tpString.rTrim( mh.getMarker(markerFactory), markerFactory).toString();
+        resultStr = tpString.rTrim(mh.getMarker(markerFactory), markerFactory).toString();
     }
 
     @Then("^return string without trailing whilespaces \"([^\"]*)\"$")
@@ -100,7 +99,7 @@ public class TpStringFeature extends TpAbsTest {
 
     @When("^\"([^\"]*)\" with whitespace in beginning or end is provided$")
     public void withWhitespaceInBeginningOrEndIsProvided(@Transform(MarkerTransform.class) final MarkerHelper mh) throws Throwable {
-        resultStr = tpString.trim( mh.getMarker(markerFactory), markerFactory).toString();
+        resultStr = tpString.trim(mh.getMarker(markerFactory), markerFactory).toString();
     }
 
     @Then("^return string with no whitespaces at start or end \"([^\"]*)\"$")
@@ -115,23 +114,23 @@ public class TpStringFeature extends TpAbsTest {
 
     @When("^\"([^\"]*)\" contains \"([^\"]*)\" ignore case$")
     public void containsIgnoreCase(@Transform(MarkerTransform.class) final MarkerHelper mh1, @Transform(MarkerTransform.class) final MarkerHelper mh2) throws Throwable {
-        result = tpString.containsIgnoreCase( mh1.getMarker(markerFactory), mh2.getMarker(markerFactory));
+        result = tpString.containsIgnoreCase(mh1.getMarker(markerFactory), mh2.getMarker(markerFactory));
     }
 
     @When("^\"([^\"]*)\" endsWith \"([^\"]*)\"$")
     public void endswith(@Transform(MarkerTransform.class) final MarkerHelper mh, @Transform(MarkerTransform.class) final MarkerHelper mh2) throws Throwable {
-        result = tpString.endsWith( mh.getMarker(markerFactory),  mh2.getMarker(markerFactory));
+        result = tpString.endsWith(mh.getMarker(markerFactory), mh2.getMarker(markerFactory));
     }
 
     @When("^\"([^\"]*)\" endsWith \"([^\"]*)\" ignore case$")
     public void endswithIgnoreCase(@Transform(MarkerTransform.class) final MarkerHelper mh, @Transform(MarkerTransform.class) final MarkerHelper mh2) throws Throwable {
-        result = tpString.endsWithIgnore( mh.getMarker(markerFactory),  mh2.getMarker(markerFactory));
+        result = tpString.endsWithIgnore(mh.getMarker(markerFactory), mh2.getMarker(markerFactory));
     }
 
     @When("^\"([^\"]*)\" is given and \"([^\"]*)\" chars to be extracted from head$")
     public void isGivenAndCharsToBeExtractedFromHead(@Transform(MarkerTransform.class) final MarkerHelper mh, String n) throws Throwable {
         try {
-            Marker resultM = tpString.extractLeading( mh.getMarker(markerFactory), Integer.parseInt(n), markerFactory);
+            Marker resultM = tpString.extractLeading(mh.getMarker(markerFactory), Integer.parseInt(n), markerFactory);
             resultStr = resultM.toString();
         } catch (RuntimeException e) {
             resultStr = "";
@@ -141,7 +140,7 @@ public class TpStringFeature extends TpAbsTest {
     @When("^\"([^\"]*)\" is given and \"([^\"]*)\" chars to be extracted from trail$")
     public void isGivenAndCharsToBeExtractedFromTrail(@Transform(MarkerTransform.class) final MarkerHelper mh, String n) throws Throwable {
         try {
-            Marker extractTrailing = tpString.extractTrailing( mh.getMarker(markerFactory), Integer.parseInt(n), markerFactory);
+            Marker extractTrailing = tpString.extractTrailing(mh.getMarker(markerFactory), Integer.parseInt(n), markerFactory);
             resultStr = extractTrailing.toString();
 
         } catch (RuntimeException e) {
@@ -151,7 +150,7 @@ public class TpStringFeature extends TpAbsTest {
 
     @When("^two markers has to be merged \"([^\"]*)\" and \"([^\"]*)\"$")
     public void twoMarkersHasToBeMergedAnd(@Transform(MarkerTransform.class) final MarkerHelper mh, @Transform(MarkerTransform.class) final MarkerHelper mh2) throws Throwable {
-        Marker merge = tpString.merge( mh.getMarker(markerFactory),  mh2.getMarker(markerFactory), markerFactory);
+        Marker merge = tpString.merge(mh.getMarker(markerFactory), mh2.getMarker(markerFactory), markerFactory);
         resultStr = merge.toString();
     }
 }
