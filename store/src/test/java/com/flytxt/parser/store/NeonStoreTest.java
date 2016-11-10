e2dc835982fd6fe11cd701ac5e0680591fec78e1
Aslam Remeez <aslam.remeez@flytxt.com>
2016-11-10 17:25:20 +0530

    store changes

---
+++ store/src/test/java/com/flytxt/parser/store/NeonStoreTest.java
@@ -0,0 +1,60 @@
+package com.flytxt.parser.store;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.flytxt.tp.marker.Marker;
+import com.flytxt.tp.marker.MarkerFactory;
+import com.flytxt.tp.store.NeonStore;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={TestSpringApplciation.class})
+@EnableScheduling
+public class NeonStoreTest {
+
+	@Autowired
+	private NeonStore store;
+	private MarkerFactory mf;
+
+	@Before
+	public void before() throws FileNotFoundException, IOException, InterruptedException {
+		NeonStore.init();
+		store = new NeonStore();
+		mf = new MarkerFactory();
+
+	}
+
+	@Test
+	public void test() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader("app/test/testFile"));
+		String string = null;
+		while ((string = reader.readLine()) != null) {
+			String[] split = string.split(",");
+			Marker[] markerArray = new Marker[split.length];
+			int i = 0;
+			for (String str : split) {
+				Marker createMarker = mf.createMarker(str);
+				markerArray[i++] = createMarker;
+
+			}
+			store.save(string.getBytes(), "test", markerArray);
+		}
+		reader.close();
+	}
+
+	@After
+	public void after() throws IOException {
+		store.done();
+	}
+}
---
+++ store/src/test/java/com/flytxt/parser/store/TestSpringApplciation.java
@@ -0,0 +1,9 @@
+package com.flytxt.parser.store;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+public class TestSpringApplciation {
+
+}
