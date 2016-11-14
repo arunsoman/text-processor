36507060f0bc53fbc24a08e81436b8b070f645be
aarunsoman <arunsoman@gmail.com>
2016-11-14 16:58:34 +0530

    added test case for streamstore

--- store/src/test/java/com/flytxt/parser/store/StreamTest.java
+++ store/src/test/java/com/flytxt/parser/store/StreamTest.java
@@ -3,6 +3,7 @@ package com.flytxt.parser.store;
 import java.io.IOException;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.flytxt.tp.marker.Marker;
@@ -16,7 +17,7 @@ public class StreamTest {
         MarkerFactory markerFactory = new MarkerFactory();
         StreamStore ss = new StreamStore("/tmp");
         ss.set("StreamTest-test");
-        String line = "1234567890,12,2,1,11,182745681129, ,1478771400000,1478771458907,1478771458907,recharge";
+        String line = "1234567890,12,2,1,11,182745681129,,1478771400000,1478771458907,1478771458907,recharge";
         String[] split = line.split("|");
         Marker[] markers = new Marker[split.length];
         int index = 0;
@@ -25,6 +26,25 @@ public class StreamTest {
             markers[index++] = markerFactory.createMarker(bytes, 0, bytes.length);
         }
         ss.save(null, "", markers);
+        Assert.assertTrue(true);
+    }
+    
+    @Test @Ignore
+    public void largeDataTest() throws IOException {
+        MarkerFactory markerFactory = new MarkerFactory();
+        StreamStore ss = new StreamStore("/tmp");
+        ss.set("StreamTest-test");
+        for(int i = 0; i < 100000; i++){
+        String line = i+"7890,12,2,1,11,182745681129,,1478771400000,1478771458907,1478771458907,recharge";
+        String[] split = line.split(",");
+        Marker[] markers = new Marker[split.length];
+        int index = 0;
+        for (String l : split) {
+            byte[] bytes = l.getBytes();
+            markers[index++] = markerFactory.createMarker(bytes, 0, bytes.length);
+        }
+        ss.save(null, "", markers);
+        }
         Assert.assertTrue(true);
     }
 }
