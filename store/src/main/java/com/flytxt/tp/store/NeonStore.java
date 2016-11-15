21b7a9392b609da56231d13f6002933c209c2262
aarunsoman <arunsoman@gmail.com>
2016-11-15 13:02:21 +0530

    adapted neonStore with FlyMemStore hdfs write

--- store/src/main/java/com/flytxt/tp/store/NeonStore.java
+++ store/src/main/java/com/flytxt/tp/store/NeonStore.java
@@ -7,6 +7,7 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.security.PrivilegedExceptionAction;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -25,34 +26,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class NeonStore implements Store {
 
-	private static MappedByteBuffer out;
-
-	// read and write indexes are stored in this buffer in ((int)readIndex,
-	// (int)writeIndex) format
-	private static MappedByteBuffer meta;
-
 	private static Semaphore semaphore = new Semaphore(1);
 
 	private static OutputStreamWriter writer;
 
-	private static final int bufSize = 1 * 1024 * 1024;
-
-	//private static byte[] data = new byte[bufSize];
-	private static byte[] data;
-
+	private static FlyMemStore fms ;
 	private static UserGroupInformation ugi = UserGroupInformation.createRemoteUser("root");
-
+	private static ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
 	@SuppressWarnings("resource")
 	public static void init() throws FileNotFoundException, IOException, InterruptedException {
-		if (out != null)
+		if (fms != null)
 			return;
 		semaphore.acquire();
-		if (out != null) {
+		if (fms != null) {
 			semaphore.release();
 			return;
 		}
-		out = new RandomAccessFile("hadoopData.dat", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, bufSize);
-		meta = new RandomAccessFile("hadoopMeta.dat", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, 8);
+		fms = new FlyMemStore();
 
 		Path path = new Path("/tmp/output");
 		Configuration config = new Configuration();
@@ -69,49 +59,25 @@ public class NeonStore implements Store {
 	@Override
 	public void save(byte[] data, String fileName, Marker... markers) throws IOException {
 		try {
-			// if (out.position() + data.length > bufSize) // checking for
-			// bufSize
-			
-			log.info(""+out.remaining()+" "+data.length);
-			if (out.remaining() <= data.length+1)
-				writeToHdfs();
-			semaphore.acquire();
-			// boundary
-			boolean needDelimiter = false;
-			for (Marker marker : markers) {
-				if (needDelimiter)
-					out.putChar(',');
-				out.put(marker.getData(), marker.index, marker.length);
-				needDelimiter = true;
-
-			}
-			if (needDelimiter)
-				out.put("\n".getBytes());
-			meta.putInt(4, out.position()); // update writeIndex in meta
-			semaphore.release();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+			rwl.readLock().lock();
+			fms.write(markers);
+			rwl.readLock().unlock();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			rwl.writeLock().lock();
+			writeToHdfs(fms.read());
+			rwl.writeLock().unlock();
 		}
 	}
 
-	private static void writeToHdfs() throws IOException {
-		int lastReadIndex = meta.getInt(0), lastWriteIndex = meta.getInt(4);
-		if (lastReadIndex == lastWriteIndex)
-			return;
+	private static void writeToHdfs(byte[] data) throws IOException {
 		try {
 			ugi.doAs(new PrivilegedExceptionAction<Void>() {
 
 				@Override
 				public Void run() throws Exception {
 					semaphore.acquire();
-					
-					out.position(lastReadIndex);
-					data= new byte[lastWriteIndex - lastReadIndex];
-					out.get(data, lastReadIndex, lastWriteIndex - lastReadIndex);
 					writer.write(data);
 					writer.close();
-					out.position(lastReadIndex);
-					meta.putInt(0, lastReadIndex); // update readIndex in meta
 					semaphore.release();
 					return null;
 				}
@@ -135,7 +101,9 @@ public class NeonStore implements Store {
 	@Scheduled(fixedDelay = 500)
 	public void timer() {
 		try {
-			writeToHdfs();
+			rwl.writeLock().lock();
+			writeToHdfs(fms.read());
+			rwl.writeLock().unlock();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
