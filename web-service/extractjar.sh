9ed426b792303143bbf4f7ddfb0302589fae63c7
aslam <aslam.remeez@flytxt.com>
2016-11-17 18:17:30 +0530

    Docker build

--- web-service/extractjar.sh
+++ web-service/extractjar.sh
@@ -7,3 +7,5 @@ var1=`jar -tvf tp.jar | grep  'store-0' | awk '{print $8}'`
 jar -xvf tp.jar $var1
 var1=`jar -tvf tp.jar | grep  'processor' | awk '{print $8}'`
 jar -xvf tp.jar $var1
+mkdir /shared-lib
+mv BOOT-INF/lib/* /shared-lib/
