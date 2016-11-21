4328596e7efbcb17b345b372cb0993a1c7793762
Arun Soman <aarunsoman@gmail.com>
2016-11-21 15:26:46 +0530

    removed unused methods from Router

--- marker/src/main/java/com/flytxt/tp/marker/Router.java
+++ marker/src/main/java/com/flytxt/tp/marker/Router.java
@@ -2,23 +2,13 @@ package com.flytxt.tp.marker;
 
 import java.util.Arrays;
 
-public final class Router implements Comparable<int[]> {
+public final class Router {
 
     private int[] markerNATPosition;
     private int[] markerPosition;//input
     private int[] indexPositionPtr;
 
-    public String toString(){
-    	StringBuilder sb = new StringBuilder();
-    	toStr(sb, "markerPosition", markerPosition);
-    	toStr(sb, "indexPositionPtr", indexPositionPtr);
-    	toStr(sb, "markerNATPosition", markerNATPosition);
-    	sb.append("summary\n");
-    	for(int i = 0; i < markerNATPosition.length; i++){
-    		sb.append("index: ").append(i).append("pick split : ").append(geNthtMarkerlocation(i)).append(" assign to : ").append(getMarkerPosition(i)).append("\n");
-    	}
-    	return sb.toString();
-    }
+
     /***
      *  546 -> markerPosition/input
      *  456=> indexPositionPtr
@@ -44,7 +34,7 @@ public final class Router implements Comparable<int[]> {
 			if(i == markerPosition[j])
 				return j;
 		}
-		throw new RuntimeException("could noy find location for "+i +"in array"+markerPosition);
+		throw new RuntimeException("could not find location for "+i +"in array"+markerPosition);
 	}
 
 	public int geNthtMarkerlocation(int index){
@@ -54,21 +44,5 @@ public final class Router implements Comparable<int[]> {
 	public int getMarkerPosition(int locationIndex){
 		return markerNATPosition[locationIndex];
 	}
-    @Override
-    public int compareTo(int[] o) {
-        if (markerPosition.length != o.length)
-            return -1;
-        for (int i = 0; i < o.length; i++)
-            if (markerPosition[i] != o[i])
-                return -1;
-        return 0;
-    }
-
-    private void toStr(StringBuilder sb,String name, int [] array){
-    	sb.append(name).append("=>");
-    	for(int i = 0; i < array.length; i++){
-    		sb.append(array[i]).append(",");
-    	}	
-    	sb.append("\n");
-    }
+    
 }
