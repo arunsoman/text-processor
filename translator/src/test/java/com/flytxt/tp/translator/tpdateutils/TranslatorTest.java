a9ec06f069abc0d40227967a1cdb3c4415b56e73
shiju.john <shiju.john@FLY-LP442.flytxt.com>
2016-11-22 18:48:22 +0530

    Junit Changes -shiju

--- translator/src/test/java/com/flytxt/tp/translator/tpdateutils/TranslatorTest.java
+++ translator/src/test/java/com/flytxt/tp/translator/tpdateutils/TranslatorTest.java
@@ -157,4 +157,25 @@ public class TranslatorTest {
 			}
 		}
 	}
+	
+	@Test
+	public void testParse(){
+		DateTime testTime =  new DateTime();
+		try {
+			Assert.assertEquals(Translator.parse(dateToString(testTime)).getDayOfMonth(),testTime.getDayOfMonth());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 * @param dt
+	 * @return
+	 */
+	private String dateToString(org.joda.time.DateTime dt) {	
+		org.joda.time.format.DateTimeFormatter fmt = org.joda.time.format.DateTimeFormat.forPattern( "ddMMyyyy HH:mm:ss.SZ");
+		String dateAsstr = fmt.print(dt);
+		return dateAsstr;
+	}
 }
