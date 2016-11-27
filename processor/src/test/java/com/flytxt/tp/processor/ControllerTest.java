687ba59e0c37acadb5adeba8fa4a0e42a850a332
aarunsoman <arunsoman@gmail.com>
2016-11-27 21:58:59 +0530

    some more tests

--- processor/src/test/java/com/flytxt/tp/processor/ControllerTest.java
+++ processor/src/test/java/com/flytxt/tp/processor/ControllerTest.java
@@ -6,28 +6,24 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 @ActiveProfiles("test")
 public class ControllerTest {
+	
+	@Autowired
+	private Controller controller;
+
 
 	@Test
-	public void testReload(){
-		int size = 5;
-		List<FlyReader> flyReaders = new ArrayList<FlyReader>(size);
-		for(int i = 0; i < size; i++){
-			FlyReader flyReader = Mockito.mock(FlyReader.class);
-			flyReaders.add(flyReader);
-		}
-		Processor mockProessor = Mockito.mock(Processor.class);
-		mockProessor.fileReaders = flyReaders;
-		
-		Controller controller = new Controller();
-		controller.processor = mockProessor;
-		
+	public void testReload() throws Exception{
 		controller.reload();
 	}
 	@Test
